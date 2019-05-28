8e79fa07d17c80b7b8d6bfc77f06678137eaf37d
Akiladhanapal <50789417+Akiladhanapal@users.noreply.github.com>
2019-05-28 03:42:47 -0700

    Add files via upload

---
+++ Swapping.java
@@ -0,0 +1,12 @@
+class Swapping
+{
+public static void main(String args[])
+{
+int num1=5,num2=7,temp;
+temp=num2;
+num2=num1;
+num1=temp;
+System.out.println(num1);
+System.out.println(num2);
+}
+}
