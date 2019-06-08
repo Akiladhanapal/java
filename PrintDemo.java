7d96ae0bcec0d5e3370a503a2b9967b6c036708e
Akiladhanapal <50789417+Akiladhanapal@users.noreply.github.com>
2019-06-08 05:43:00 -0700

    Add files via upload

---
+++ PrintDemo.java
@@ -0,0 +1,19 @@
+class PrintDemo
+{
+public static void main(String args[])
+{
+	int i,j,rows=4,num=1;
+	for(i=1;i<=rows;i++)
+	{
+	for(j=1;j<=i;j++)
+	{
+	System.out.print(num+ " ");
+	++num;
+	}
+	System.out.println();
+	}
+	}
+	}
+	
+	
+	
